f388bf808893eb6d563f61f8e3b6ab03a2ab641d
Csanad Marton <csanad.marton@gmail.com>
2022-02-16 21:19:04 +0100

    setting speed

--- game.js
+++ game.js
@@ -65,7 +65,7 @@ function nextSequence() {
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeIn(150).fadeOut(150).fadeIn(150);
     playSound(randomChosenColour);
 
     console.log(gamePattern);
